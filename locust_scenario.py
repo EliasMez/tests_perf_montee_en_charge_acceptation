f9581dedfb7718ef6dcb915a3626608455225425
EliasMez <elias.mezine@laposte.net>
2023-08-31 14:56:07 +0200

    ajout github action

--- locust_scenario.py
+++ locust_scenario.py
@@ -2,7 +2,7 @@ from locust import HttpUser, task, between
 from items_api import Item
 
 class MonScenario(HttpUser):
-    host = "http://127.0.0.1:8000" 
+    host = "http://127.0.0.1:8000"
     wait_time = between(1, 5)
 
     @task
@@ -13,3 +13,5 @@ class MonScenario(HttpUser):
     def soumettre_formulaire(self):
         data = {'name':"test", 'description':"test"}
         self.client.post("/items", json=data)
+
+ 
